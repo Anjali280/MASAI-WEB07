85eea0f939f0e4c859cc7bce5ae77274a82d1d7f
Anjali280 <mondalanjali0707@gmail.com>
2023-03-03 23:09:28 +0530

    Post Method

---
+++ HTTP Methods/postMethods.js
@@ -0,0 +1,11 @@
+fetch("https://jsonplaceholder.typicode.com/todos", {
+  method: "POST",
+  headers: { "content-type": "application/json" },
+  body: JSON.stringify({
+    userId: 6,
+    id: 300,
+    title: "Learn fetch API",
+  }),
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data));
